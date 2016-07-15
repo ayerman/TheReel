73b5f43d7834614f48b0920dd81614fe3e3a64fd
ayerman <ayerman@kent.edu>
2016-07-14 22:39:46 -0400

    topics controller added

--- Server/TheReelWeb/TheReelWeb/Models/DBContext.cs
+++ Server/TheReelWeb/TheReelWeb/Models/DBContext.cs
@@ -2,11 +2,12 @@
 using System.Data.Entity;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using TheReelWeb.Models;
 
 namespace TheReelWeb.Models
 {
     [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
-    public class UsersContext : DbContext
+    public class DBContext : DbContext
     {
         // You can add custom code to this file. Changes will not be overwritten.
         // 
@@ -15,11 +16,11 @@ namespace TheReelWeb.Models
         // For more information refer to the documentation:
         // http://msdn.microsoft.com/en-us/data/jj591621.aspx
 
-        public UsersContext() : base("name=reel")
+        public DBContext() : base("name=reel")
         {
         }
 
-        static UsersContext()
+        static DBContext()
         {
             // static constructors are guaranteed to only fire once per application.
             // I do this here instead of App_Start so I can avoid including EF
@@ -27,6 +28,7 @@ namespace TheReelWeb.Models
             DbConfiguration.SetConfiguration(new MySql.Data.Entity.MySqlEFConfiguration());
         }
 
-        public System.Data.Entity.DbSet<TheReelWeb.Models.Users> Users { get; set; }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Topic> Topic { get; set; }
     }
 }
