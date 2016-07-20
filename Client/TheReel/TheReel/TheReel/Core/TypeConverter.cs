1df63526a50f22a07e5ff532fa4eb2975ddfc557
Bill Hoag <Billyhoa007@gmail.com>
2016-07-19 20:52:02 -0400

    Added files for image component

---
+++ Client/TheReel/TheReel/TheReel/Core/TypeConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheReel.Core
+{
+    public abstract class TypeConverter
+    {
+        public virtual bool CanConvertFrom(Type sourceType)
+        {
+            if (sourceType == null)
+                throw new ArgumentNullException("sourceType");
+
+            return sourceType == typeof(string);
+        }
+
+        [Obsolete("use ConvertFromInvariantString (string)")]
+        public virtual object ConvertFrom(object o)
+        {
+            return null;
+        }
+
+        [Obsolete("use ConvertFromInvariantString (string)")]
+        public virtual object ConvertFrom(CultureInfo culture, object o)
+        {
+            return null;
+        }
+
+        public virtual object ConvertFromInvariantString(string value)
+        {
+#pragma warning disable 0618 // retain until ConvertFrom removed
+            return ConvertFrom(CultureInfo.InvariantCulture, value);
+#pragma warning restore
+        }
+    }
+}
